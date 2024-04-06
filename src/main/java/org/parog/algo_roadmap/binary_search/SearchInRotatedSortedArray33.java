6d3b226de0a111ddbe173d42783fa4390629672a
alexanderbibik <sbibik2000@mail.ru>
2024-04-06 12:32:08 +0300

    binary search: SearchInRotatedSortedArray33

--- src/main/java/org/parog/algo_roadmap/binary_search/SearchInRotatedSortedArray33.java
+++ src/main/java/org/parog/algo_roadmap/binary_search/SearchInRotatedSortedArray33.java
@@ -13,14 +13,61 @@ package org.parog.algo_roadmap.binary_search;
  * Ограничение по памяти:
  */
 public class SearchInRotatedSortedArray33 {
-    public int search(int[] nums, int target) {
+    public static int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
+        int ans = -1;
 
         while (left <= right) {
-
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) return mid;
+            // гарантируем, что левый подмассив отсортирован
+            if (nums[left] <= nums[mid]) {
+                if (isTargetInLeftSortedArr(nums, target, mid, left)) {
+                    // исключаем левую половину
+                    right = mid - 1;
+                } else {
+                    // исключаем правую половину
+                    left = mid + 1;
+                }
+                // гарантируем, что правый подмассив отсортирован
+            } else {
+                if (isTargetInRightSortedArr(nums, target, mid, right)) {
+                    // исключаем левую половину
+                    left = mid + 1;
+                } else {
+                    // исключаем правую половину
+                    right = mid - 1;
+                }
+            }
         }
 
-        return left;
+        return ans;
+    }
+
+    /**
+     * Находится ли target в левой половине, сравнивая ее краем левой границы и средним элементами.
+     *
+     * @param nums   массив чисел
+     * @param target искомый элемент
+     * @param mid    текущая середина
+     * @param left   левая граница
+     * @return true - в отсортированной половине, false - в смещенной половине.
+     */
+    private static boolean isTargetInLeftSortedArr(int[] nums, int target, int mid, int left) {
+        return target <= nums[mid] && target >= nums[left];
+    }
+
+    /**
+     * Находится ли target в правой половине, сравнивая ее краем правой границы и средним элементами.
+     *
+     * @param nums   массив чисел
+     * @param target искомый элемент
+     * @param mid    текущая середина
+     * @param right  левая граница
+     * @return true - в отсортированной половине, false - в смещенной половине.
+     */
+    private static boolean isTargetInRightSortedArr(int[] nums, int target, int mid, int right) {
+        return target >= nums[mid] && target <= nums[right];
     }
 }
--- src/test/java/org/parog/algo_roadmap/binary_search/SearchInRotatedSortedArray33Test.java
+++ src/test/java/org/parog/algo_roadmap/binary_search/SearchInRotatedSortedArray33Test.java
@@ -1,9 +1,26 @@
 package org.parog.algo_roadmap.binary_search;
 
-import org.parog.algo_roadmap.binary_search.SearchInRotatedSortedArray33;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Тестовый класс для задачи {@link SearchInRotatedSortedArray33}
  */
 public class SearchInRotatedSortedArray33Test {
+
+    @Test
+    void SearchInRotatedSortedArray33Test1() {
+        assertThat(SearchInRotatedSortedArray33.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0)).isEqualTo(4);
+    }
+
+    @Test
+    void SearchInRotatedSortedArray33Test2() {
+        assertThat(SearchInRotatedSortedArray33.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 3)).isEqualTo(-1);
+    }
+
+    @Test
+    void SearchInRotatedSortedArray33Test3() {
+        assertThat(SearchInRotatedSortedArray33.search(new int[]{1}, 0)).isEqualTo(-1);
+    }
 }
