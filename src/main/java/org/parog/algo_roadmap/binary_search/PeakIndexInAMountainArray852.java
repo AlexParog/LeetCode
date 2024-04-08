24c002c908f3a59276fa02efa2305a0fe712a5d3
alexanderbibik <sbibik2000@mail.ru>
2024-04-08 22:22:11 +0300

    binary search: PeakIndexInAMountainArray852

--- src/main/java/org/parog/algo_roadmap/binary_search/PeakIndexInAMountainArray852.java
+++ src/main/java/org/parog/algo_roadmap/binary_search/PeakIndexInAMountainArray852.java
@@ -1,4 +1,81 @@
 package org.parog.algo_roadmap.binary_search;
 
+/**
+ * 1.
+ * Диапазон arr.length: 3 <= arr.length <= 10^5
+ * Диапазон arr[i]: 0 <= arr[i] <= 10^6
+ * arr - это гарантированно mountain array.
+ * 2.
+ * Тестовый класс {@link PeakIndexInAMountainArray852Test}
+ * 3.
+ * Ограничение по времени: O(logN) - бинарный поиск
+ * Ограничение по памяти: O(1) - константное время, так как алгоритм использует только фиксированное количество переменных,
+ * независимо от размера входного массива arr
+ */
 public class PeakIndexInAMountainArray852 {
+    public static int peakIndexInMountainArray(int[] arr) {
+        if (arr.length < 3) return -1;
+
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (mid > 0 && mid < arr.length - 1) {
+                if (isPeak(arr, mid)) {
+                    return mid;
+                } else if (arr[mid] < arr[mid + 1]) { // Если элемент справа от mid больше, значит мы в восходящей части
+                    left = mid + 1;
+                } else { // Иначе, мы в нисходящей части
+                    right = mid;
+                }
+            } else {
+                // Если мы на границах, peak по определению быть не может
+                return -1;
+            }
+        }
+        // После выхода из цикла left будет указывать на peak
+        return left;
+    }
+
+    /**
+     * Является ли текущее положением вершиной
+     *
+     * @param arr массив
+     * @param mid средний элемент
+     * @return true - да, false - находимся на uphill/downhill
+     */
+    private static boolean isPeak(int[] arr, int mid) {
+        return arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1];
+    }
+
+    /**
+     * Более простая задача: является ли переданный массив "mountain array".
+     *
+     * @param arr массив
+     * @return true / false
+     */
+    private static boolean validMountainArray(int[] arr) {
+        if (arr.length < 3) return false;
+
+        int n = arr.length, uphill = 0, downhill = n - 1;
+        // идем в гору
+        while (uphill < n - 1) {
+            if (arr[uphill] < arr[uphill + 1]) uphill++;
+            else break;
+        }
+
+        // пик не обнаружен
+        if (uphill == n - 1) return false;
+
+        // спускаемся с горы
+        while (downhill > 0) {
+            if (arr[downhill] < arr[downhill - 1]) downhill--;
+            else break;
+        }
+        // пик не обнаружен
+        if (downhill == 0) return false;
+
+        // единая вершина
+        return uphill == downhill;
+    }
 }
--- src/test/java/org/parog/algo_roadmap/binary_search/PeakIndexInAMountainArray852Test.java
+++ src/test/java/org/parog/algo_roadmap/binary_search/PeakIndexInAMountainArray852Test.java
@@ -1,9 +1,58 @@
 package org.parog.algo_roadmap.binary_search;
 
-import org.parog.algo_roadmap.binary_search.PeakIndexInAMountainArray852;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Тестовый класс для задачи {@link PeakIndexInAMountainArray852}
  */
 public class PeakIndexInAMountainArray852Test {
+    @Test
+    void PeakIndexInAMountainArray852Test1() {
+        assertThat(PeakIndexInAMountainArray852.peakIndexInMountainArray(new int[]{0, 1, 0}))
+                .isEqualTo(1);
+    }
+
+    @Test
+    void PeakIndexInAMountainArray852Test2() {
+        assertThat(PeakIndexInAMountainArray852.peakIndexInMountainArray(new int[]{0, 2, 1, 0}))
+                .isEqualTo(1);
+    }
+
+    @Test
+    void PeakIndexInAMountainArray852Test3() {
+        assertThat(PeakIndexInAMountainArray852.peakIndexInMountainArray(new int[]{0, 10, 5, 2}))
+                .isEqualTo(1);
+    }
+
+    @Test
+    void PeakIndexInAMountainArray852Test4() {
+        assertThat(PeakIndexInAMountainArray852.peakIndexInMountainArray(new int[]{0, 2, 3, 4, 5, 2, 1, 0}))
+                .isEqualTo(4);
+    }
+
+    @Test
+    void PeakIndexInAMountainArray852Test5() {
+        assertThat(PeakIndexInAMountainArray852.peakIndexInMountainArray(new int[]{0, 1, 2, 3, 4, 5, 6, 7}))
+                .isEqualTo(-1);
+    }
+
+    @Test
+    void PeakIndexInAMountainArray852Test6() {
+        assertThat(PeakIndexInAMountainArray852.peakIndexInMountainArray(new int[]{7, 6, 5, 4, 3, 2, 1, 0}))
+                .isEqualTo(-1);
+    }
+
+    @Test
+    void PeakIndexInAMountainArray852Test7() {
+        assertThat(PeakIndexInAMountainArray852.peakIndexInMountainArray(new int[]{18, 29, 38, 59, 98, 100, 99, 98, 90}))
+                .isEqualTo(5);
+    }
+
+    @Test
+    void PeakIndexInAMountainArray852Test8() {
+        assertThat(PeakIndexInAMountainArray852.peakIndexInMountainArray(new int[]{3, 5, 3, 2, 0}))
+                .isEqualTo(1);
+    }
 }
