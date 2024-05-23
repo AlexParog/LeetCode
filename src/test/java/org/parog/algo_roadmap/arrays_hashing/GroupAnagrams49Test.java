65081cb6935108cd5a01df4ac9d5d070a8d6691e
alexanderbibik <sbibik2000@mail.ru>
2024-05-23 12:47:44 +0300

    arrays and hashing : GroupAnagrams49 added solution with HashCode

--- src/test/java/org/parog/algo_roadmap/arrays_hashing/GroupAnagrams49Test.java
+++ src/test/java/org/parog/algo_roadmap/arrays_hashing/GroupAnagrams49Test.java
@@ -12,9 +12,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GroupAnagrams49Test {
     @Test
     void GroupAnagrams49Test1() {
-        assertThat(GroupAnagrams49.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}))
+        assertThat(GroupAnagrams49.groupAnagramsWithHashCode(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}))
                 .isEqualTo(List.of(List.of("eat", "tea", "ate"),
                         List.of("bat"),
                         List.of("tan", "nat")));
+    }
+
+    @Test
+    void GroupAnagrams49Test2() {
+        assertThat(GroupAnagrams49.groupAnagramsWithMap(new String[]{}))
+                .isEqualTo(List.of(List.of()));
     }
 }
