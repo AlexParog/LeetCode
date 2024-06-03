b8f6bb98350ee723db92fd72377457e77cd55fed
alexanderbibik <sbibik2000@mail.ru>
2024-06-03 22:01:52 +0300

    arrays and hashing : right decision ProductOfArrayExceptSelf238

--- src/main/java/org/parog/algo_roadmap/arrays_hashing/ProductOfArrayExceptSelf238.java
+++ src/main/java/org/parog/algo_roadmap/arrays_hashing/ProductOfArrayExceptSelf238.java
@@ -1,32 +1,72 @@
 package org.parog.algo_roadmap.arrays_hashing;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 1.
+ * Диапазон длины: 2 <= nums.length <= 10^5
+ * Диапазон значений элементов массива: -30 <= nums[i] <= 30
+ * Произведение любого prefix или suffix nums гарантированно поместится в 32-битное целое число
  * 2.
- * 3.
+ * Тестовый класс {@link ProductOfArrayExceptSelf238Test}
  */
 public class ProductOfArrayExceptSelf238 {
 
-    // TL
-    // решение с помощью префиксов и постфиксов
-    public static int[] productExceptSelf(int[] nums) {
-        List<Integer> result = new ArrayList<>();
-        int miss = 0;
+    /**
+     * Временная сложность: O(n), где n количество элементов в массиве nums
+     * Пространственная сложность: O(n), так как созданы два дополнительных массива для префиксов и постфиксов размера
+     * nums.length
+     *
+     * @param nums входной массив
+     * @return массив, где nums[i] равен произведению всех элементов nums, кроме nums[i]
+     */
+    public static int[] productExceptSelfWithExtraSpace(int[] nums) {
+        int[] result = new int[nums.length];
 
-        while (miss < nums.length) {
-            int el = 1;
-            for (int i = 0; i < nums.length; i++) {
-                if (i == miss){
-                    continue;
-                }
-                el *= nums[i];
-            }
-            result.add(el);
-            miss++;
+        int[] prefix = new int[nums.length];
+        int left = 1;
+        // префиксные значения
+        for (int i = 0; i < nums.length; i++) {
+            prefix[i] = left;
+            left *= nums[i];
+
         }
-        return result.stream().mapToInt(i -> i).toArray();
+
+        int[] postfix = new int[nums.length];
+        int right = 1;
+        // постфиксные значения
+        for (int j = nums.length - 1; j >= 0; j--) {
+            postfix[j] = right;
+            right *= nums[j];
+        }
+
+        for (int index = 0; index < nums.length; index++) {
+            result[index] = prefix[index] * postfix[index];
+        }
+
+        return result;
+    }
+
+    /**
+     * Временная сложность: O(n), где n количество элементов в массиве nums
+     * Пространственная сложность: O(1), так как используем алгоритм in place (выходной result не считается при оценке
+     * сложности алгоритма)
+     *
+     * @param nums входной массив
+     * @return массив, где nums[i] равен произведению всех элементов nums, кроме nums[i]
+     */
+    public static int[] productExceptSelfWithInPlace(int[] nums) {
+        int[] result = new int[nums.length];
+        int left = 1, right = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = left;
+            left *= nums[i];
+        }
+
+        for (int j = nums.length - 1; j >= 0; j--) {
+            result[j] *= right;
+            right *= nums[j];
+        }
+
+        return result;
     }
 }
--- src/test/java/org/parog/algo_roadmap/arrays_hashing/ProductOfArrayExceptSelf238Test.java
+++ src/test/java/org/parog/algo_roadmap/arrays_hashing/ProductOfArrayExceptSelf238Test.java
@@ -10,13 +10,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ProductOfArrayExceptSelf238Test {
     @Test
     void ProductOfArrayExceptSelf238Test1() {
-        assertThat(ProductOfArrayExceptSelf238.productExceptSelf(new int[]{1, 2, 3, 4}))
+        assertThat(ProductOfArrayExceptSelf238.productExceptSelfWithExtraSpace(new int[]{1, 2, 3, 4}))
                 .isEqualTo(new int[]{24, 12, 8, 6});
     }
 
     @Test
     void ProductOfArrayExceptSelf238Test2() {
-        assertThat(ProductOfArrayExceptSelf238.productExceptSelf(new int[]{-1, 1, 0, -3, 3}))
+        assertThat(ProductOfArrayExceptSelf238.productExceptSelfWithInPlace(new int[]{-1, 1, 0, -3, 3}))
                 .isEqualTo(new int[]{0, 0, 9, 0, 0});
     }
 }
