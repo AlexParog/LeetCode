130e74efc1bd875baf98e92af6b642c41ed81622
alexanderbibik <sbibik2000@mail.ru>
2024-06-06 13:11:27 +0300

    arrays and hashing : right test method CheckIfEveryRowAndColumnContainsAllNumbers2133

--- src/test/java/org/parog/algo_roadmap/arrays_hashing/CheckIfEveryRowAndColumnContainsAllNumbers2133Test.java
+++ src/test/java/org/parog/algo_roadmap/arrays_hashing/CheckIfEveryRowAndColumnContainsAllNumbers2133Test.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CheckIfEveryRowAndColumnContainsAllNumbers2133Test {
     @Test
     void CheckIfEveryRowAndColumnContainsAllNumbers2133Test1() {
-        assertThat(CheckIfEveryRowAndColumnContainsAllNumbers2133.checkValidWithMatrixRotation(new int[][]{
+        assertThat(CheckIfEveryRowAndColumnContainsAllNumbers2133.checkValid(new int[][]{
                 {1, 2, 3},
                 {3, 1, 2},
                 {2, 3, 1}})).isEqualTo(true);
@@ -18,7 +18,7 @@ public class CheckIfEveryRowAndColumnContainsAllNumbers2133Test {
 
     @Test
     void CheckIfEveryRowAndColumnContainsAllNumbers2133Test2() {
-        assertThat(CheckIfEveryRowAndColumnContainsAllNumbers2133.checkValidWithMatrixRotation(new int[][]{
+        assertThat(CheckIfEveryRowAndColumnContainsAllNumbers2133.checkValid(new int[][]{
                 {1, 1, 1},
                 {1, 2, 3},
                 {1, 2, 3}})).isEqualTo(false);
@@ -26,7 +26,7 @@ public class CheckIfEveryRowAndColumnContainsAllNumbers2133Test {
 
     @Test
     void CheckIfEveryRowAndColumnContainsAllNumbers2133Test3() {
-        assertThat(CheckIfEveryRowAndColumnContainsAllNumbers2133.checkValidWithMatrixRotation(new int[][]{
+        assertThat(CheckIfEveryRowAndColumnContainsAllNumbers2133.checkValid(new int[][]{
                 {1, 2, 3},
                 {1, 3, 2},
                 {3, 2, 1}})).isEqualTo(false);
