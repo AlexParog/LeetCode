d84a2e9d8542fb0344793a7dcd52d13a1aea8c7f
Alex Parog <sbibik2000@mail.ru>
2025-03-07 17:41:31 +0300

    feat: add solution for MergeSortedArray88

--- src/main/java/org/parog/algo_roadmap/two_pointers/MergeSortedArray88.java
+++ src/main/java/org/parog/algo_roadmap/two_pointers/MergeSortedArray88.java
@@ -14,6 +14,45 @@ package org.parog.algo_roadmap.two_pointers;
  * Пространственная сложность: O(1), так как никакие структуры данных дополнительно не используются
  */
 public class MergeSortedArray88 {
+
+    /**
+     * Можно избавиться от дополнительного цикла while, добавив условие indexNum1 >= 0, которое необходимо для проверки
+     * границы массива nums1. То есть по суть в одном цикле while две подзадачи: вставка элементов в порядке возрастания
+     * и копирование оставшихся элементов, которые не обработали.
+     *
+     * @param nums1 входящий массив nums1
+     * @param m     количество элементов заполненных элементов
+     * @param nums2 входящий массив nums2
+     * @param n     количество элементов заполненных элементов
+     * @return объединенный отсортированный в порядке возрастания массив
+     */
+    public static int[] optimizedMerge(int[] nums1, int m, int[] nums2, int n) {
+        int indexNum1 = m - 1;
+        int indexNum2 = n - 1;
+        int insert = m + n - 1;
+
+        while (indexNum2 >= 0) {
+            if (indexNum1 >= 0 && nums1[indexNum1] > nums2[indexNum2]) {
+                nums1[insert--] = nums1[indexNum1--];
+            } else {
+                nums1[insert--] = nums2[indexNum2--];
+            }
+        }
+
+        return nums1;
+    }
+
+    /**
+     * Разделение на две подзадачи и явная проверка двух указателей, чтобы не вышли за рамки массивов nums1 и nums2.
+     * Код становится более понятным, так как явно выделены подзадачи: вставка элементов в порядке возрастания
+     * и копирование оставшихся элементов, которые не обработали.
+     *
+     * @param nums1 входящий массив nums1
+     * @param m     количество элементов заполненных элементов
+     * @param nums2 входящий массив nums2
+     * @param n     количество элементов заполненных элементов
+     * @return объединенный отсортированный в порядке возрастания массив
+     */
     public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
         int indexNums1 = m - 1;
         int indexNums2 = n - 1;
@@ -26,7 +65,7 @@ public class MergeSortedArray88 {
             else
                 nums1[insertionPosition--] = nums1[indexNums1--];
         }
-        
+
         // копирует оставшиеся элементы из nums2 в начало nums1
         while (indexNums2 >= 0)
             nums1[insertionPosition--] = nums2[indexNums2--];
--- src/test/java/org/parog/algo_roadmap/two_pointers/MergeSortedArray88Test.java
+++ src/test/java/org/parog/algo_roadmap/two_pointers/MergeSortedArray88Test.java
@@ -10,31 +10,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MergeSortedArray88Test {
     @Test
     void MergeSortedArray88Test1() {
-        assertThat(MergeSortedArray88.merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3))
+        assertThat(MergeSortedArray88.optimizedMerge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3))
                 .isEqualTo(new int[]{1, 2, 2, 3, 5, 6});
     }
 
     @Test
     void MergeSortedArray88Test2() {
-        assertThat(MergeSortedArray88.merge(new int[]{1}, 1, new int[]{}, 0))
+        assertThat(MergeSortedArray88.optimizedMerge(new int[]{1}, 1, new int[]{}, 0))
                 .isEqualTo(new int[]{1});
     }
 
     @Test
     void MergeSortedArray88Test3() {
-        assertThat(MergeSortedArray88.merge(new int[]{0}, 0, new int[]{1}, 1))
+        assertThat(MergeSortedArray88.optimizedMerge(new int[]{0}, 0, new int[]{1}, 1))
                 .isEqualTo(new int[]{1});
     }
 
     @Test
     void MergeSortedArray88Test4() {
-        assertThat(MergeSortedArray88.merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{1, 4, 6}, 3))
+        assertThat(MergeSortedArray88.optimizedMerge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{1, 4, 6}, 3))
                 .isEqualTo(new int[]{1, 1, 2, 3, 4, 6});
     }
 
     @Test
     void MergeSortedArray88Test5() {
-        assertThat(MergeSortedArray88.merge(new int[]{4, 5, 6, 0, 0, 0}, 3, new int[]{1, 2, 3}, 3))
+        assertThat(MergeSortedArray88.optimizedMerge(new int[]{4, 5, 6, 0, 0, 0}, 3, new int[]{1, 2, 3}, 3))
                 .isEqualTo(new int[]{1, 2, 3, 4, 5, 6});
     }
 }
